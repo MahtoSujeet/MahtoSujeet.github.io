ffe9c029ab55ad54ed86dd747f5c9ddab320a2ed
Sujeet-Mahto <sujeetmehta6@gmail.com>
2023-04-11 00:07:14 +0530

    added hamburger nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,23 +1,27 @@
 import "../css/Nav.css"
+import { Spin as Hamburger } from 'hamburger-react'
 
-import React from "react"
+import React, { useState } from "react"
 import Logo from "./Logo.jsx"
 
 export default function Nav() {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const closeMenu = () => setIsOpen(false)
+
   return (
     <nav className="Nav">
       <Logo />
-      <div className="NavOpts">
-        <NavOpt link="#" name="Home" />
-        <NavOpt link="#Skills" name="Skills" />
-        <NavOpt link="#ContactMe" name="Contact Me" />
+      <div className={`NavOpts ${isOpen && "open"}`}>
+        <a className="navOpt" href="#" onClick={closeMenu}>Home</a>
+        <a className="navOpt" href="#Skills" onClick={closeMenu}>Skills</a>
+        <a className="navOpt" href="#ContactMe" onClick={closeMenu}>Contact Me</a>
+      </div>
+      <div className="hamburgerWrapper">
+        <Hamburger toggled={isOpen} toggle={setIsOpen} size={25} rounded />
       </div>
     </nav >
   )
 }
 
-function NavOpt(props) {
-  return (
-    <a className="NavOpt" href={props.link}>{props.name}</a>
-  )
-}
+// #TODO add transtion time, and auto add class when button is clicked
